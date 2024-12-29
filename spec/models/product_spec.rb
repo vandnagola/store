b1e5423908c3dad86dcd6fb6e9f005f59c675076
vandnagola <vandnagola756@gmail.com>
2024-12-29 16:42:35 +0530

    write test case

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it "Product chyeck" do 
-    expect(false).to be(false)
+  it "Product name should be present" do 
+    pro = Product.new(name:"dummy")
+    pro.save
+    expect(pro).to be_valid
   end
-  
+
+  it "is not valid without name" do 
+    pro = Product.new(name: nil)
+    pro.save
+    expect(pro).not_to be_valid
+  end  
 end

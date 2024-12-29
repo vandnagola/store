ecfc26b35abecbe2ae3895fc30ba7834a8d6cea5
vandnagola <vandnagola756@gmail.com>
2024-12-29 17:18:56 +0530

    fix CI/CD error

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,15 +1,15 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Product, type: :model do
-  it "Product name should be present" do 
-    pro = Product.new(name:"dummy")
+  it "Product name should be present" do
+    pro = Product.new(name: "dummy")
     pro.save
     expect(pro).to be_valid
   end
 
-  it "is not valid without name" do 
+  it "is not valid without name" do
     pro = Product.new(name: nil)
     pro.save
     expect(pro).not_to be_valid
-  end  
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,29 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  subject {User.new(name:"dummy", age: 24, city:"dehli", email:"dummy@124gmail.com")}
+  subject { User.new(name: "dummy", age: 24, city: "dehli", email: "dummy@124gmail.com") }
 
   it "is valid with attributes" do
     expect(subject).to be_valid
   end
 
-  it "is not valid without name " do 
+  it "is not valid without name" do
     subject.name = nil
     expect(subject).not_to be_valid
   end
 
   context "when age is greater than 18" do
-    let(:age){20}
+    let(:age) { 20 }
 
-    it "is valid" do 
+    it "is valid" do
       expect(subject).to be_valid
     end
   end
-  context "when age is less than 18"
-    it "is not valid" do 
+  context "when age is less than 18" do
+    it "is not valid" do
       subject.age = 17
       expect(subject).not_to be_valid
       expect(subject.errors[:age]).to include("must be greater than 18")
     end
   end
-
 end

4be8c020bc49aba53fad42c9ed4c52190965dad8
vandnagola <vandnagola756@gmail.com>
2025-01-26 19:59:03 +0530

    generate comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_29_054119) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_26_142816) do
+  create_table "comments", force: :cascade do |t|
+    t.text "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
